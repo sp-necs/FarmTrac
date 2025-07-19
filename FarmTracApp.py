3b1f862b38981f53010fa4f84b0a4cb04674989d
sp-necs <suprasad7.1996@gmail.com>
2025-07-19 14:25:12 +0100

    Updated add activity and search activity

--- FarmTracApp.py
+++ FarmTracApp.py
@@ -9,19 +9,8 @@ st.subheader("Welcome to FarmTrac!")
 st.write("FarmTrac app helps to keep track of farm work activities. Please select from following options:")
 AddFarmActivity,SearchFarmActivity,AddWorker,AddFarm = st.tabs(["Add Farm Activity","Search Farm Activity","Add Farm Worker","Add Farm"])
 
-with AddFarmActivity:
-    WorkerData = pd.read_csv("datasets\WorkerDim.csv")
-    WorkerList = WorkerData["WorkerName"]
-    FarmData = pd.read_csv("datasets\FarmDim.csv")
-    FarmList = FarmData["FarmName"]
-    AddFarmActivityForm = st.form("Add Farm Activity")
-    WorkDate = AddFarmActivityForm.date_input(label="Work Date", max_value=dt.today(), value="today")
-    FarmName = AddFarmActivityForm.selectbox(label="Farm",options=FarmList)
-    WorkerName = AddFarmActivityForm.multiselect(label="Farm Workers",options=WorkerList)
-    AddFarmActivityBtn = AddFarmActivityForm.form_submit_button("Add Activity")
-
 with AddFarm:
-    AddFarmForm = st.form("Add Farm")
+    AddFarmForm = st.form("Add Farm",clear_on_submit=True)
     FarmName = AddFarmForm.text_input("Enter Farm Name:")
     AddFarmBtn = AddFarmForm.form_submit_button("Add Farm")
     if AddFarmBtn:
@@ -47,7 +36,7 @@ with AddFarm:
                 st.success(f"Success: {FarmName} added!")
 
 with AddWorker:
-    AddWorkerForm = st.form("Add Worker")
+    AddWorkerForm = st.form("Add Worker",clear_on_submit=True)
     WorkerName = AddWorkerForm.text_input("Enter Worker Name:")
     AddWorkerBtn = AddWorkerForm.form_submit_button("Add Worker")
     if AddWorkerBtn:
@@ -70,3 +59,52 @@ with AddWorker:
                     writerObj = writer(f)
                     writerObj.writerow(NewWorker)
                 st.success(f"Success: {WorkerName} added!")
+
+with AddFarmActivity:
+    WorkerData = pd.read_csv("datasets\WorkerDim.csv")
+    WorkerList = WorkerData["WorkerName"]
+    FarmData = pd.read_csv("datasets\FarmDim.csv")
+    FarmList = FarmData["FarmName"]
+    AddFarmActivityForm = st.form("Add Farm Activity",clear_on_submit=True)
+    WorkDate = AddFarmActivityForm.date_input(label="Work Date", max_value=dt.today(), value="today")
+    FarmName = AddFarmActivityForm.selectbox(label="Farm",options=FarmList,index=None)
+    WorkerName = AddFarmActivityForm.multiselect(label="Farm Workers",options=WorkerList)
+    Description = AddFarmActivityForm.text_input("Work Description")
+    AddFarmActivityBtn = AddFarmActivityForm.form_submit_button("Add Activity")
+    if AddFarmActivityBtn:
+        if FarmName == None or WorkerName == '':
+            st.error("Error: Please enter the Farm and Worker details!")
+        else:
+            NewFarmWorkId = 0
+            FarmWorkData = pd.read_csv("datasets\FarmWork.csv")
+            if FarmWorkData.empty:
+                NewFarmWorkId = 1
+            else:
+                NewFarmWorkId = FarmWorkData["FarmWorkId"].max() + 1
+            for worker in WorkerName:
+                NewFarmWork = [NewFarmWorkId, WorkDate, FarmName, worker, Description]
+                with open("datasets\FarmWork.csv",'a',newline='') as f:
+                    writerObj = writer(f)
+                    writerObj.writerow(NewFarmWork)
+                NewFarmWorkId = NewFarmWorkId + 1
+            st.success("Success: Farm Activity added!")
+
+with SearchFarmActivity:
+    FarmActivityData = pd.read_csv("datasets\FarmWork.csv")
+    FarmData = pd.read_csv("datasets\FarmDim.csv")
+    FarmActivityData["FarmWorkDate"] = pd.to_datetime(FarmActivityData["FarmWorkDate"]).dt.date
+    SearchFarmActivityForm = st.form("Search Farm Activity")
+    StartDate = SearchFarmActivityForm.date_input(label="From Date", max_value=dt.today(),value="today")
+    EndDate = SearchFarmActivityForm.date_input(label="To Date", max_value=dt.today(),value="today")
+    SearchFarmName = SearchFarmActivityForm.selectbox(label="Farm Name",options=FarmData["FarmName"],index=None)
+    SearchFarmActivityBtn = SearchFarmActivityForm.form_submit_button("Search Activity")
+    if SearchFarmActivityBtn:
+        if StartDate>EndDate:
+            st.error("Error: Please enter correct dates!")
+        else:
+            if FarmActivityData.empty:
+                st.error("Error: There is no farm activity data!")
+            else:
+                if SearchFarmName == None:
+                    SearchResult = FarmActivityData[(FarmActivityData["FarmWorkDate"]>=StartDate) & (FarmActivityData["FarmWorkDate"]<=EndDate)]
+                    SearchFarmActivity.write(SearchResult)
